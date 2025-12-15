9bf063a65a0e1ef5172bb2a53be69b2fa7953ebd
soekhant01 <soekhant.dev@gmail.com>
2025-12-15 23:21:46 +0630

    created specific databaseBuilder for android and ios

---
+++ composeApp/src/androidMain/kotlin/com/example/et_note/getDatabaseBuilder.kt
@@ -0,0 +1,12 @@
+package com.example.et_note
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.et_note.db.NoteDatabase
+
+fun getDatabaseBuilder(context: Context): RoomDatabase.Builder<NoteDatabase>{
+    val appContext = context.applicationContext
+    val dbPath = appContext.getDatabasePath("note_database.db")
+    return Room.databaseBuilder(appContext, NoteDatabase::class.java, dbPath.path)
+}
---
+++ composeApp/src/iosMain/kotlin/com/example/et_note/getDatabaseBuilder.kt
@@ -0,0 +1,29 @@
+package com.example.et_note
+
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.et_note.db.NoteDatabase
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSHomeDirectory
+import platform.Foundation.NSUserDomainMask
+
+
+fun getDatabaseBuilder(): RoomDatabase.Builder<NoteDatabase>{
+    val dbPath = getDocument() + "/note_database.db"
+    return Room.databaseBuilder(name = dbPath)
+}
+
+@OptIn(ExperimentalForeignApi::class)
+fun getDocument(): String{
+
+    val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
+        directory = NSDocumentDirectory,
+        inDomain = NSUserDomainMask,
+        appropriateForURL = null,
+        create = false,
+        error = null
+    )
+    return requireNotNull(documentDirectory?.path)
+}
