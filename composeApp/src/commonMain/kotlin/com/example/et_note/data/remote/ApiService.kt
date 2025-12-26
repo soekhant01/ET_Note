6ddf834003bdeae519bc84dcb6a47cfe242cbae9
soekhant01 <soekhant.dev@gmail.com>
2025-12-26 16:10:09 +0630

    singin&up req res function implemented

--- composeApp/src/commonMain/kotlin/com/example/et_note/data/remote/ApiService.kt
+++ composeApp/src/commonMain/kotlin/com/example/et_note/data/remote/ApiService.kt
@@ -2,13 +2,42 @@ package com.example.et_note.data.remote
 
 import com.example.et_note.model.AuthRequest
 import com.example.et_note.model.AuthResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.HttpStatusCode
 
 expect val BASE_URL_EMULATOR: String
 const val BASE_URL = "https://localhost:8080"
 
-class ApiService {
+class ApiService(val client: HttpClient) {
+    private val ACTIVE_URL = BASE_URL_EMULATOR
+    private val LOGIN_ENDPOINT = "${ACTIVE_URL}/auth/login"
+    private val SIGNUP_ENDPOINT = "${ACTIVE_URL}/auth/signup"
+    suspend fun login(request: AuthRequest): Result<AuthResponse>{
+        return try {
+            val response = client.post(urlString =  LOGIN_ENDPOINT){
+                setBody(request)
+            }
+            if (response.status == HttpStatusCode.OK) Result.success(response.body() as AuthResponse)
+            else Result.failure(Exception("Failed to load data"))
+        }catch (ex: Exception){
+            Result.failure(ex)
+        }
 
-    fun login(request: AuthRequest): Result<AuthResponse>{
+    }
+
+    suspend fun signup(request: AuthRequest): Result<AuthResponse>{
+        return try {
+            val response = client.post(urlString =  SIGNUP_ENDPOINT){
+                setBody(request)
+            }
+            if (response.status == HttpStatusCode.Created) Result.success(response.body() as AuthResponse)
+            else Result.failure(Exception("Failed to load data"))
+        }catch (ex: Exception){
+            Result.failure(ex)
+        }
 
     }
 }
