5f9bde7007a93806ecc292446c758cfaa9ebfc30
soekhant01 <soekhant.dev@gmail.com>
2025-12-26 20:31:47 +0630

    signIn ui and data implemented

--- composeApp/src/commonMain/kotlin/com/example/et_note/feature/auth/SignInScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/et_note/feature/auth/SignInScreen.kt
@@ -8,10 +8,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -19,74 +21,139 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun SignInScreen(navController: NavController) {
-    val viewModel = viewModel { SignUpViewModel() }
+    val viewModel = viewModel { SignInViewModel() }
 
     val email = viewModel.email.collectAsStateWithLifecycle()
     val password = viewModel.password.collectAsStateWithLifecycle()
+    val uiState = viewModel.uiState.collectAsStateWithLifecycle()
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text("Sign Up", fontSize = 22.sp)
-        Spacer(Modifier.size(16.dp))
+    LaunchedEffect(true) {
+
+        viewModel.navigationFlow.collectLatest {
+            when (it) {
+                is AuthNavigation.NavigateToHome -> {
+                    navController.getBackStackEntry("home").savedStateHandle.set(
+                        "email",
+                        email.value
+                    )
+                    navController.popBackStack("home", inclusive = false)
+                }
+            }
+        }
+    }
+
+    when (uiState.value) {
+
+        is AuthState.Loading -> {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CircularProgressIndicator()
+                Text("Loading...")
+            }
+        }
+
+        is AuthState.Failure -> {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Successful: ${(uiState.value as AuthState.Failure).error}")
+                Button(
+                    onClick = { viewModel.onErrorClick() }
+                ) { Text("Retry") }
+            }
+        }
+
+        is AuthState.Success -> {
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                val email = (uiState.value as AuthState.Success).response.email
+                Text("Successful: $email")
+                Button(
+                    onClick = { viewModel.onSuccessClick(email) }
+                ) { Text("Go Back") }
+            }
+
+        }
+
+        is AuthState.Normal -> {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Sign In", fontSize = 22.sp)
+                Spacer(Modifier.size(16.dp))
 
 //        email text field
-        OutlinedTextField(
-            email.value,
-            onValueChange = {
-                viewModel.onEmailUpdate(it)
-            },
-            modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp),
-            placeholder = {
-                Text("Email")
-            },
-            label = {
-                Text("Email")
-            }
+                OutlinedTextField(
+                    email.value,
+                    onValueChange = {
+                        viewModel.onEmailUpdate(it)
+                    },
+                    modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp),
+                    placeholder = {
+                        Text("Email")
+                    },
+                    label = {
+                        Text("Email")
+                    }
 
-        )
-        Spacer(Modifier.size(16.dp))
+                )
+                Spacer(Modifier.size(16.dp))
 
 //        password text field
-        OutlinedTextField(
-            password.value,
-            onValueChange = {
-                viewModel.onPasswordUpdate(it)
-            },
-            modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp),
-            placeholder = {
-                Text("Password")
-            },
-            label = {
-                Text("Password")
+                OutlinedTextField(
+                    password.value,
+                    onValueChange = {
+                        viewModel.onPasswordUpdate(it)
+                    },
+                    modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp),
+                    placeholder = {
+                        Text("Password")
+                    },
+                    label = {
+                        Text("Password")
+                    }
+
+                )
+                Spacer(Modifier.size(16.dp))
+
+
+                Spacer(Modifier.size(16.dp))
+
+                TextButton(
+                    {
+                        navController.popBackStack()
+                    },
+                ){
+                    Text("Don't have an account? Login")
+                }
+                Spacer(Modifier.size(16.dp))
+
+                Button(onClick = {
+                    viewModel.signIn()
+                }, modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp)){
+                    Text("Submit")
+                }
+
+
             }
-
-        )
-        Spacer(Modifier.size(16.dp))
-
-
-        Spacer(Modifier.size(16.dp))
-
-        TextButton(
-            {
-                navController.popBackStack()
-            },
-        ){
-            Text("Don't have an account? Login")
         }
-        Spacer(Modifier.size(16.dp))
-
-        Button(onClick = {
-            viewModel.signUp()
-        }, modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp)){
-            Text("Submit")
-        }
-
-
     }
+
+
+
 }
--- composeApp/src/commonMain/kotlin/com/example/et_note/feature/auth/SignInViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/example/et_note/feature/auth/SignInViewModel.kt
@@ -1,8 +1,15 @@
 package com.example.et_note.feature.auth
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.et_note.data.remote.ApiService
+import com.example.et_note.data.remote.HttpClientFactory
+import com.example.et_note.model.AuthRequest
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class SignInViewModel: ViewModel() {
     private val _email = MutableStateFlow<String>("")
@@ -10,6 +17,28 @@ class SignInViewModel: ViewModel() {
 
     private val _password = MutableStateFlow<String>("")
     val password = _password.asStateFlow()
+
+
+    private val apiService = ApiService(HttpClientFactory.getHttpClient())
+    private val _uiState = MutableStateFlow<AuthState>(AuthState.Normal)
+    val uiState = _uiState.asStateFlow()
+
+    private val _navigationFlow = MutableSharedFlow<AuthNavigation>()
+    val navigationFlow = _navigationFlow.asSharedFlow()
+
+    fun onErrorClick(){
+
+        viewModelScope.launch {
+            _uiState.value = AuthState.Normal
+        }
+    }
+
+    fun onSuccessClick(email: String){
+
+        viewModelScope.launch {
+            _navigationFlow.emit(AuthNavigation.NavigateToHome(email))
+        }
+    }
 
     fun onEmailUpdate(email: String){
         _email.value = email
@@ -21,6 +50,12 @@ class SignInViewModel: ViewModel() {
 
 
     fun signIn(){
-
+        viewModelScope.launch {
+            val request = AuthRequest(email.value, password.value)
+            _uiState.value = AuthState.Loading
+            val result = apiService.login(request)
+            if(result.isSuccess) _uiState.value = AuthState.Success(result.getOrNull()!!)
+            else AuthState.Failure(result.exceptionOrNull()?.message.toString())
+        }
     }
 }
