821ca522acc975468e912b24f10ddbe536b306a2
soekhant01 <soekhant.dev@gmail.com>
2025-12-27 11:51:20 +0630

    datastore manager implemented

---
+++ composeApp/src/commonMain/kotlin/com/example/et_note/data/cache/DataStoreManager.kt
@@ -0,0 +1,66 @@
+package com.example.et_note.data.cache
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+
+class DataStoreManager(private val dataStore: DataStore<Preferences>) {
+
+    companion object {
+        private val TOKEN_KEY = stringPreferencesKey("token")
+        private val EMAIL_KEY = stringPreferencesKey("email")
+        private val REFRESH_KEY = stringPreferencesKey("refresh")
+        private val USER_ID_KEY = stringPreferencesKey("user_id")
+    }
+
+//    store string
+    suspend fun storeString(key: Preferences.Key<String>, value: String) {
+
+        dataStore.updateData { preferences ->
+            preferences.toMutablePreferences().apply {
+                this[key] = value
+            }
+        }
+    }
+
+    suspend fun storeToken(token: String) {
+        storeString(TOKEN_KEY, token)
+    }
+
+    suspend fun storeEmail(email: String) {
+        storeString(EMAIL_KEY, email)
+    }
+
+    suspend fun storeRefresh(refresh: String) {
+        storeString(REFRESH_KEY, refresh)
+    }
+
+    suspend fun storeUserId(userId: String){
+        storeString(USER_ID_KEY, userId)
+    }
+//    get string
+    suspend fun getString(key: Preferences.Key<String>): String? {
+
+        return dataStore.data.map { it[key] }.first()
+    }
+
+    suspend fun getToken(): String? {
+        return getString(TOKEN_KEY)
+    }
+
+    suspend fun getEmail(): String? {
+        return getString(EMAIL_KEY)
+    }
+
+    suspend fun getRefresh(): String? {
+        return getString(REFRESH_KEY)
+    }
+
+    suspend fun getUseId(): String? {
+        return getString(USER_ID_KEY)
+    }
+
+
+}
